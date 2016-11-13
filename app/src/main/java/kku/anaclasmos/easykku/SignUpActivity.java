a10b5349432f381babbf671b2ea07f91a99e4e23
anaclasmos <art@kkumail.com>
2016-11-13 11:19:33 +0700

    design main

--- app/src/main/java/kku/anaclasmos/easykku/SignUpActivity.java
+++ app/src/main/java/kku/anaclasmos/easykku/SignUpActivity.java
@@ -1,9 +1,14 @@
 package kku.anaclasmos.easykku;
 
+import android.content.Context;
 import android.content.Intent;
+import android.database.Cursor;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.net.Uri;
+import android.os.AsyncTask;
+import android.os.StrictMode;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -11,6 +16,17 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.squareup.okhttp.FormEncodingBuilder;
+import com.squareup.okhttp.OkHttpClient;
+import com.squareup.okhttp.Request;
+import com.squareup.okhttp.RequestBody;
+import com.squareup.okhttp.Response;
+
+import org.jibble.simpleftp.SimpleFTP;
+
+import java.io.File;
 
 public class SignUpActivity extends AppCompatActivity {
 
@@ -20,9 +36,11 @@ public class SignUpActivity extends AppCompatActivity {
 
     private ImageView imageView;
     private Button button;
-    private String nameString, phoneString, userString, passwordString;
+    private String nameString, phoneString, userString, passwordString, imagePathString, imageNameString;
     private Uri uri;
-
+    private boolean aBoonlean = true;
+    private String urlAddUser = "http://swiftcodingthai.com/kku/add_user_master.php";
+    private String urlImage = "http://swiftcodingthai.com/kku/Image";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -57,8 +75,17 @@ public class SignUpActivity extends AppCompatActivity {
                     Log.d("12novV1", "Have Space");
                     MyAlert myAlert = new MyAlert(SignUpActivity.this, R.drawable.doremon48, "มีช่องว่าง", "กรุณากรอกให้ครบทุกช่องครับ");
                     myAlert.myDialog();
+                } else if (aBoonlean){
+                    //Non Choose Image
+                    MyAlert myAlert = new MyAlert(SignUpActivity.this, R.drawable.nobita48, "ยังไม่เลือกรูป", "กรุณาเลือกรูปด้วยครับ");
+                    myAlert.myDialog();
+                }else{
+                    //Choose Image Ok
+                    upLoadImageToServer();
+                    upLoadStringToServer();
 
                 }
+
             }   //OnClick
         });
 
@@ -76,6 +103,96 @@ public class SignUpActivity extends AppCompatActivity {
 
     } //main method
 
+    private void upLoadStringToServer() {
+
+        AddNewUser addNewUser=new AddNewUser(SignUpActivity.this);
+        addNewUser.execute(urlAddUser);
+
+    }   //upload string to server
+
+    // create inner class
+    private class AddNewUser extends AsyncTask<String, Void, String> {
+
+        //Explicit
+        private Context context;
+        public  AddNewUser(Context context){
+            this.context = context;
+        }
+
+        @Override
+        protected String doInBackground(String... params) {
+
+            try{
+
+                OkHttpClient okHttpClient = new OkHttpClient();
+                RequestBody requestBody = new FormEncodingBuilder()
+                        .add("isAdd", "true")
+                        .add("Name", nameString)
+                        .add("Phone", phoneString)
+                        .add("User", userString)
+                        .add("Password", passwordString)
+                        .add("Image", urlImage + imageNameString)
+                        .build();
+
+                Request.Builder builder = new  Request.Builder();
+                Request request = builder.url(params[0]).post(requestBody).build();
+                Response response = okHttpClient.newCall(request).execute();
+                return response.body().string();
+
+            }catch (Exception e){
+                Log.d("13NovV1", "e doIn ==> "+ e.toString());
+                return null;
+
+            }
+
+
+
+
+        }   //doInBack ททำงานอยุ่เบื้องหลัง ำหน้าที่ต่อเน็ต
+
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+
+
+            Log.d("13novV1", "Result ==> " + s);
+
+            if(Boolean.parseBoolean(s)){
+                Toast.makeText(context, "Uplolad Success",Toast.LENGTH_SHORT).show();
+                finish();
+            }else {
+                Toast.makeText(context, "Cannot Uplolad Success",Toast.LENGTH_SHORT).show();
+
+            }
+
+        }   //OnPost ทำงานหลัง DoItBack ทำงานเสร็จ
+    }   // AddNewUser class
+
+
+    private  void  upLoadImageToServer(){
+
+        // Change Policy
+        StrictMode.ThreadPolicy threadPolicy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+        StrictMode.setThreadPolicy(threadPolicy);
+
+        try{
+            SimpleFTP simpleFTP = new SimpleFTP();
+            simpleFTP.connect("ftp.swiftcodingthai.com",21,"kku@swiftcodingthai.com","Abc12345");
+            simpleFTP.bin();
+            simpleFTP.cwd("Image");
+            simpleFTP.stor(new File(imagePathString));
+            simpleFTP.disconnect();
+
+
+        }catch (Exception e){
+            Log.d("12novV1", "e simpleFTP ==> " + e.toString());
+        }
+
+
+
+        //upload
+    }
+
     @Override
     protected void onActivityResult(int requestCode,
                                     int resultCode,
@@ -84,18 +201,47 @@ public class SignUpActivity extends AppCompatActivity {
 
         if ((requestCode == 0) && (resultCode == RESULT_OK)) {
             Log.d("12novV1","Result OK");
-
+            aBoonlean = false;
             // Show image
             uri = data.getData();
             try {
+
                 Bitmap bitmap = BitmapFactory.decodeStream(getContentResolver().openInputStream(uri));
                 imageView.setImageBitmap(bitmap);
 
             }catch (Exception e){
                 e.printStackTrace();
-
             }
+
+            //Find Path of Image
+            imagePathString = myFindPath(uri);
+            Log.d("12novV1", "imagePath ==>" + imagePathString);
+
+
+            //Find Name of Image
+            imageNameString = imagePathString.substring(imagePathString.lastIndexOf("/"));
+            Log.d("12novV1", "imageName ==> " + imageNameString);
+
+
         }   //if
 
     }   // on Activity result
+
+    private String myFindPath(Uri uri) {
+        String result = null;
+        String[] strings = {MediaStore.Audio.Media.DATA};
+        Cursor cursor = getContentResolver().query(uri, strings, null, null, null);
+
+        if(cursor != null){
+            cursor.moveToFirst();
+            int index = cursor.getColumnIndexOrThrow(MediaStore.Audio.Media.DATA);
+            result = cursor.getString(index);
+        }else{
+            result = uri.getPath();
+        }
+
+
+
+        return result;
+    }
 } //main class
